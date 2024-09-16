15719935ff81fbf2ef60a6161e5dae6a5645d90b
Valderi Douglas <valderi.douglas@pro4tech.com.br>
2024-09-15 21:06:37 -0300

    comentarios

--- dois/lib/main.dart
+++ dois/lib/main.dart
@@ -42,80 +42,59 @@ class MyAppState extends State<MyApp> {
           title: const Text('API Response Demo'),
         ),
         body: ListView.builder(
-          itemCount: dataList.length,
+          itemCount: dataList.length, // Quantidade de itens na lista.
           itemBuilder: (BuildContext context, int index) {
-            final item = dataList[index];
+            final item = dataList[index]; // Obtem o item atual da lista.
+
+            // Retorna um container com estilo condicional baseado no ID.
             return Container(
-                margin:
-                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                decoration: BoxDecoration(
-                  color: item['id'] % 2 == 0
-                      ? const Color.fromARGB(255, 16, 235, 16)
-                      : const Color.fromARGB(255, 15, 180, 205),
-                  borderRadius: BorderRadius.circular(10.0), // Raio
+              margin:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              decoration: BoxDecoration(
+                color: item['id'] % 2 == 0
+                    ? const Color.fromARGB(255, 16, 235, 16)
+                    : const Color.fromARGB(
+                        255, 15, 180, 205), // Cores alternadas.
+                borderRadius: BorderRadius.circular(10.0), // Borda arredondada.
+              ),
+              child: ListTile(
+                title: Text('Name: ${item['name']}'), // Nome do usuário.
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Username: ${item['username']}',
+                        style: TextStyle(fontSize: 16)),
+                    Text('Email: ${item['email']}',
+                        style: TextStyle(fontSize: 16)),
+                    Text('Phone: ${item['phone']}',
+                        style: TextStyle(fontSize: 16)),
+                    Text('Website: ${item['website']}',
+                        style: TextStyle(fontSize: 16)),
+                    const Divider(
+                        color: Colors.black, thickness: 1), // Divisória visual.
+                    Text('Company: ${item['company']['name']}',
+                        style: TextStyle(fontSize: 16)),
+                    Text('Catch Phrase: ${item['company']['catchPhrase']}',
+                        style: TextStyle(fontSize: 16)),
+                    Text('BS: ${item['company']['bs']}',
+                        style: TextStyle(fontSize: 16)),
+                    const Divider(color: Colors.black, thickness: 1),
+                    Text('Street: ${item['address']['street']}',
+                        style: TextStyle(fontSize: 16)),
+                    Text('Suite: ${item['address']['suite']}',
+                        style: TextStyle(fontSize: 16)),
+                    Text('City: ${item['address']['city']}',
+                        style: TextStyle(fontSize: 16)),
+                    Text('Zipcode: ${item['address']['zipcode']}',
+                        style: TextStyle(fontSize: 16)),
+                    Text('Longitude: ${item['address']['geo']['lng']}',
+                        style: TextStyle(fontSize: 12)),
+                    Text('Latitude: ${item['address']['geo']['lat']}',
+                        style: TextStyle(fontSize: 12)),
+                  ],
                 ),
-                child: ListTile(
-                  title: Text('Name: ${item['name']}'),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Username: ${item['username']}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      Text(
-                        'Email: ${item['email']}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      Text(
-                        'Phone: ${item['phone']}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      Text(
-                        'Website: ${item['website']}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'Company: ${item['company']['name']}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      Text(
-                        'Catch Phrase: ${item['company']['catchPhrase']}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      Text(
-                        'BS: ${item['company']['bs']}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'Street: ${item['address']['street']}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      Text(
-                        'Suite: ${item['address']['suite']}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      Text(
-                        'City: ${item['address']['city']}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      Text(
-                        'Zipcode: ${item['address']['zipcode']}',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      Text(
-                        'Longitude: ${item['address']['geo']['lng']}',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                      Text(
-                        'Latitude: ${item['address']['geo']['lat']}',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                    ],
-                  ),
-                ));
+              ),
+            );
           },
         ),
       ),
@@ -124,5 +103,5 @@ class MyAppState extends State<MyApp> {
 }
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp()); // Inicia o aplicativo.
 }
